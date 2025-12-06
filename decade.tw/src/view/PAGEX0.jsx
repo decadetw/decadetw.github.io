75d6cbe5968b87033791e650fc69d80ae57d48ae
x <x@x>
2025-12-07 03:45:54 +0800

    vite v1

--- decade.tw/src/view/PAGEX0.jsx
+++ decade.tw/src/view/PAGEX0.jsx
@@ -12,41 +12,52 @@ const contentStyle = {
     textAlign: 'center',
     background: '#000',
 };
+
 const list_content_youtube = [
     {t: '基隆山海鳴光', d: '', url: 'https://www.youtube.com/embed/GGZHxkKLopg?feature=oembed&showinfo=0&controls=0'},
-    {
-        t: '台北燈節主燈偶控系統',
-        d: '史上第一個會跳舞的主燈！2021台北燈節「七彩八寶新世界」12/17 登場（台北市政府提供）',
-        url: 'https://www.youtube.com/embed/et_1mErdoKY?feature=oembed'
-    },
-    {
-        t: '2021台灣燈會主燈_乘風逐光(完整版)',
-        d: '台灣燈會新竹主燈中央控制系統',
-        url: 'https://www.youtube.com/embed/GGZHxkKLopg?feature=oembed'
-    },
-    {t: '高雄流行音樂中心', d: '', url: 'https://www.youtube.com/embed/GGZHxkKLopg?feature=oembed'},
-    {
-        t: '豪華朗機工「聆聽花開的聲音」',
-        d: '豪華朗機工「聆聽花開的聲音」- 光落物生︱未來城市@天下雜誌',
-        url: 'https://www.youtube.com/embed/GGZHxkKLopg?feature=oembed'
-    },
-    {t: '誠品30週年機械手臂控制', d: '', url: 'https://www.youtube.com/embed/GGZHxkKLopg?feature=oembed'},
-    {t: '台電大樓馬達控制系統', d: '', url: 'https://www.youtube.com/embed/GGZHxkKLopg?feature=oembed'},
-    {
-        t: '2016 公共藝術作品 河飄風_Flowingly Blows the Breeze',
-        d: '',
-        url: 'https://www.youtube.com/embed/Xkbk8A6MBmI?feature=oembed'
-    },
+    {t: '台北燈節主燈偶控系統',d: '',url: 'https://www.youtube.com/embed/et_1mErdoKY?feature=oembed'},
+    {t: '台灣燈會新竹主燈中央控制系統',d: '',url: 'https://www.youtube.com/embed/RtN9LXy0nSY?feature=oembed'},
+    {t: '高雄流行音樂中心', d: '', url: 'https://www.youtube.com/embed/9_RAmF9dAQo?feature=oembed'},
+    {t: '豪華朗機工「聆聽花開的聲音」',d: '',url: 'https://www.youtube.com/embed/AMcD4XxWp9Y?feature=oembed'},
+    {t: '誠品30週年機械手臂控制', d: '', url: 'https://www.youtube.com/embed/uDib2sl9EQ4?feature=oembed'},
+    {t: '台電大樓馬達控制系統', d: '', url: 'https://www.youtube.com/embed/YPILezM36Mk?feature=oembed'},
+    {t: '台中世界花卉博覽會', d: '', url: 'https://www.youtube.com/embed/zv084TmAvKo?feature=oembed'},
+    {t: '台電大樓風動電路設計', d: '', url: 'https://www.youtube.com/embed/Xkbk8A6MBmI?feature=oembed'},
+    {t: '世大運開幕式', d: '', url: 'https://www.youtube.com/embed/mnyQ6473jj0?feature=oembed'},
+    {t: '台灣科技園區裝置控制', d: '', url: 'https://www.youtube.com/embed/XWcwRYwDcVs?feature=oembed'},
+    {t: '台南燈會中央控制系統', d: '', url: 'https://www.youtube.com/embed/uWbwuabvpgY?feature=oembed'},
 ]
+const list_content_algorithm_youtube = [
+    {t: '誠品３０週年機械手臂控制', d: '', url: 'https://www.youtube.com/embed/blTlSpL5gPA?feature=oembed&showinfo=0&controls=0'},
+    {t: '法國IRCAM 中央控制',d: '',url: 'https://www.youtube.com/embed/G21wHvVLtVY?feature=oembed'},
+    {t: '台北流行音樂中心公共藝術控制系統',d: '',url: 'https://www.youtube.com/embed/dF1C0cCYdGA?feature=oembed'},
+    // {t: '博裝置中控手機', d: '', url: 'https://www.youtube.com/embed/7zb-bQNcHsE?feature=oembed'},
+    {t: '台北燈節主燈牛偶控制系統', d: '', url: 'https://www.youtube.com/embed/iLRVEH9zFao?feature=oembed'},
+    {t: '基隆車站控制系統',d: '',url: 'https://www.youtube.com/embed/yNvj24m-KbA?feature=oembed'},
+    {t: '台南奇美博物館', d: '', url: 'https://www.youtube.com/embed/7wjWHlk9EHU?feature=oembed'},
+    {t: '台灣電力公司日光域控制系統', d: '', url: 'https://www.youtube.com/embed/0wHT3ey_Dkc?feature=oembed'},
+    {t: '新竹台灣燈會主燈控制系統', d: '', url: 'https://www.youtube.com/embed/RPPHKiupq58?feature=oembed'},
+    {t: 'Arduino教父', d: '', url: 'https://www.youtube.com/embed/b9wYHVMWJ10?feature=oembed'},
+];
+const justifyOptions = [
+    'flex-start',
+    'center',
+    'flex-end',
+    'space-between',
+    'space-around',
+    'space-evenly',
+];
+const alignOptions = ['flex-start', 'center', 'flex-end'];
 const StaticHTML = () => {
-    const {
-        token: {colorBgContainer, borderRadiusLG},
+    const {token: {colorBgContainer, borderRadiusLG},
     } = theme.useToken();
+    const [justify, setJustify] = React.useState(justifyOptions[3]);
+    const [alignItems, setAlignItems] = React.useState(alignOptions[0]);
     return (<>
             <Divider style={{
                 fontSize: '1em', fontStyle: 'italic', fontWeight: 'bold',
                 borderColor: '#ffffff'
-            }}> ...</Divider>
+            }}> ＤＥＣＡＤＥ.TW since 2008</Divider>
             <Carousel autoplay arrows>
                 {Array.from({length: 21}, (value, index) => index).map((e) => {
                     return <div>
@@ -79,10 +90,10 @@ const StaticHTML = () => {
                     fontSize: '1em', fontStyle: 'italic', fontWeight: 'bold',
                     borderColor: '#ffffff'
                 }}>提供展場軟硬體整合設計規劃；舞台燈光控制</Divider>
-                <Flex wrap gap="small">
+                <Flex wrap gap="small" justify={justify} align={alignItems}>
                     {list_content_youtube.map(e => (
                         <Card hoverable
-                              style={{width: '30vw',}}
+                              style={{width: `${100/4.3}vw`,}}
                               cover={
                                   <div className="wrapper">
 
@@ -108,18 +119,27 @@ const StaticHTML = () => {
                     fontSize: '1em', fontStyle: 'italic', fontWeight: 'bold',
                     borderColor: '#ffffff'
                 }}>軟/體/硬/電子電路/多媒體</Divider>
-                <Flex wrap gap="small">
-                    {list_content_youtube.map(e => (
+                <Flex wrap gap="small" justify={justify} align={alignItems}>
+                    {list_content_algorithm_youtube.map(e => (
                         <Card hoverable
-                              style={{width: '30vw',}}
-                              cover={
-                                  <Image ></Image>
+                              style={{width: `${100/3.2}vw`,}}
+                              cover={<div className="wrapper">
+
+                                  <div className="frame-container">
+                                      <iframe height="315"
+                                              src={e.url}
+                                              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                              referrerPolicy="strict-origin-when-cross-origin"
+                                              allowFullScreen></iframe>
+                                  </div>
+                              </div>
                               }>
                             <Meta title={e.t} description={e.d}/>
 
                         </Card>
                     ))}
                 </Flex>
+
                 <Divider style={{
                     fontSize: '3em', fontStyle: 'italic', fontWeight: 'bold',
                     borderColor: '#000000'
@@ -128,31 +148,12 @@ const StaticHTML = () => {
                     fontSize: '1em', fontStyle: 'italic', fontWeight: 'bold',
                     borderColor: '#ffffff'
                 }}>常設／半永久性裝置</Divider>
-                <Flex wrap gap="small">
-                    {list_content_youtube.map(e => (
-                        <Card hoverable
-                              style={{width: '30vw',}}
-                              cover={
-                                  <Image ></Image>
-                              }>
-                            <Meta title={e.t} description={e.d}/>
-
-                        </Card>
-                    ))}
-                </Flex>
-                <Divider style={{
-                    fontSize: '3em', fontStyle: 'italic', fontWeight: 'bold',
-                    borderColor: '#000000'
-                }}>ML/AI/軟體演算法設計</Divider>
-                <Divider style={{
-                    fontSize: '1em', fontStyle: 'italic', fontWeight: 'bold',
-                    borderColor: '#ffffff'
-                }}>effect algorithm / model training / fineTune</Divider>
-                <Flex wrap gap="small">
+                <Flex wrap gap="small" justify={justify} align={alignItems}>
                     {Array.from({length: 21}, (value, index) => index).map((e) => {
                         return <Card hoverable
-                                     style={{width: '30vw',}}
-                                     cover={
+                                     style={{width: `${100/3.2}vw`,
+                                         boxShadow: '0px 1px 10px rgba(0,1,1,0.15)'
+                                     }}                                     cover={
                                          <Image
                                              width={`100%`}
                                              alt="basic image"
@@ -176,22 +177,30 @@ const StaticHTML = () => {
                     fontSize: '1em', fontStyle: 'italic', fontWeight: 'bold',
                     borderColor: '#ffffff'
                 }}>effect algorithm / model training / fineTune</Divider>
-                <Flex wrap gap="small">
+                <Flex wrap gap="small" justify={justify} align={alignItems}>
                     {Array.from({length: 9}, (value, index) => index).map((e) => {
                         return <Card hoverable
                                      // bodyStyle={{padding: "0"}}
-                                     style={{margin: "0",width: '30vw',height: '20vw',
-                                         // boxShadow: '0px 1px 10px rgba(0,1,1,0.15)'
+                                     style={{width: `${100/3.2}vw`,
+                                         boxShadow: '0px 1px 10px rgba(0,1,1,0.15)'
                                      }}
-                                     // cover={ }
-                        >
+                                     cover={
+                                         <Image
+                                             width={`100%`}
+                                             alt="basic image"
+                                             src={`algorithmPic/${e}.png`}
+                                             preview={{
+                                                 src: `slide/${e}.png`,
+                                             }}
+                                         />
+                                     }>
                             {/*<Meta title={e.t} description={e.d}/>*/}
-                            <Image
-                                src={`algorithmPic/${e}.png`}
-                                preview={{
-                                    src: `slide/${e}.png`,
-                                }}
-                            />
+                            {/*<Image*/}
+                            {/*    src={`algorithmPic/${e}.png`}*/}
+                            {/*    preview={{*/}
+                            {/*        src: `slide/${e}.png`,*/}
+                            {/*    }}*/}
+                            {/*/>*/}
                         </Card>
                     })}
 
