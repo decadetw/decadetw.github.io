14439b84d6e3acb20616bb4cde9cf9a5cfc4f511
x <x@x>
2025-12-08 13:34:13 +0800

    vite base domain www.xxx

--- decade.tw/vite.config.js
+++ decade.tw/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: 'https://decade.tw/decade.tw/dist/',
+    base: 'https://www.decade.tw/decade.tw/dist/',
 })
